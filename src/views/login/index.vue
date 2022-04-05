73b7cfe202db7c1b207c40e47e9314653e725d3b
崔子兰 <springzilan@gmail.com>
2022-04-05 16:55:33 +0800

    feat:实现登录+注册

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,15 +1,36 @@
 <template>
   <div>
-    <input type="text" v-model="loginForm.username" placeholder="用户名"/>
-    <input type="text" v-model="loginForm.password" placeholder="密码"/>
-    <button @click="login">登录</button>
+    <div>
+      <input v-model="username"/>
+      <input v-model="password"/>
+      <button @click="show">click</button>
+    </div>
+    <div>
+      <input type="text" v-model="loginForm.username" placeholder="用户名"/>
+      <input type="text" v-model="loginForm.password" placeholder="密码"/>
+      <button @click="login">登录</button>
+    </div>
+    <div>
+      <input type="text" v-model="regist.username" placeholder="用户名"/>
+      <input type="text" v-model="regist.password" placeholder="密码"/>
+      <input type="text" v-model="regist.email" placeholder="email"/>
+      <button @click="register">注册</button>
+    </div>
   </div>
 </template>
 <script>
+// import getAll from '.'
+const axios = require('axios')
 export default {
   data () {
     return {
+      username: '',
+      password: '',
       loginForm: {
+        username: '',
+        password: ''
+      },
+      regist: {
         username: '',
         password: ''
       },
@@ -17,7 +38,37 @@ export default {
     }
   },
   methods: {
+    register () {
+      var that = this
+      axios.post('http://localhost:8080/user/register', {
+        // 请求注册接口
+        username: this.regist.username,
+        password: this.regist.password,
+        email: this.regist.email
+      }).then(function (response) {
+        that.response = response.data
+        // that.$emit('lisentcurrent', [that.response])
+      }).catch(function (error) {
+        console.log(error)
+      })
+    },
     login () {
+      var that = this
+      axios.post('http://localhost:8080/user/login', {
+        // 请求登录接口
+        username: this.loginForm.username,
+        password: this.loginForm.password
+      }).then(function (response) {
+        // console.log(response.data);
+        that.response = response.data
+        // that.$emit('lisentcurrent', [that.response])
+      }).catch(function (error) {
+        console.log(error)
+      })
+      console.log('pass', that.response)
+      console.log('click me')
+    },
+    show () {
       console.log('登录中')
     }
   }
